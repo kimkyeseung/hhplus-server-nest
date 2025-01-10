42d56cf473e28111105ce6d6f1f1ed3c5b40956b
Kim Kyeseung <chickenboys@naver.com>
2025-01-10 09:07:52 +0900

    fix: 포인트 모듈 오류 수정

--- src/points/points.module.ts
+++ src/points/points.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PointsService } from './points.service';
 import { PointsController } from './points.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Point } from './entities/point.entity';
+import { PointHistory } from './entities/point-history.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Point, PointHistory])],
   controllers: [PointsController],
   providers: [PointsService],
 })
